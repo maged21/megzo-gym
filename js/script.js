9638c1eab100f40ba49b1bafd933b4c2d4b0cbd3
Maged Abdulaziz <majedabdulaziz72@yahoo.com>
2023-11-30 13:18:54 +0200

    header

--- js/script.js
+++ js/script.js
@@ -93,3 +93,14 @@ function accordion(ele) {
     })
 }
 accordion(".accordion");
+
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+  
